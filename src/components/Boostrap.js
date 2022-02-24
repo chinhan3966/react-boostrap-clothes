fea7aa51772adfb8e2cdc5227231c32d1e657c77
chinhan3966 <chinhan3966@gmail.com>
2022-02-24 09:11:09 +0700

    first-commit

---
+++ src/components/Boostrap.js
@@ -0,0 +1,147 @@
+import React from "react";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+const Boostrap = () => {
+  return (
+    <div className="mt-4">
+      <Container>
+        <Row>
+          <Col sm={12} md={6} lg={4}>
+            <div class="card mb-4">
+              <img
+                src="https://media-cdn.laodong.vn/Storage/NewsPortal/2022/2/8/1002117/Ca-Si-Justin-Bieber.jpg"
+                class="card-img-top"
+                alt="..."
+              />
+
+              <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </p>
+                <a href="#" class="btn btn-primary">
+                  Go somewhere
+                </a>
+              </div>
+            </div>
+          </Col>
+          <Col sm={12} md={6} lg={4}>
+            <div class="card">
+              <img
+                src="https://media-cdn.laodong.vn/Storage/NewsPortal/2022/2/8/1002117/Ca-Si-Justin-Bieber.jpg"
+                class="card-img-top"
+                alt="..."
+              />
+
+              <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </p>
+                <a href="#" class="btn btn-primary">
+                  Go somewhere
+                </a>
+              </div>
+            </div>
+          </Col>
+          <Col sm={12} md={6} lg={4}>
+            <div class="card">
+              <img
+                src="https://media-cdn.laodong.vn/Storage/NewsPortal/2022/2/8/1002117/Ca-Si-Justin-Bieber.jpg"
+                class="card-img-top"
+                alt="..."
+              />
+
+              <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </p>
+                <a href="#" class="btn btn-primary">
+                  Go somewhere
+                </a>
+              </div>
+            </div>
+          </Col>
+          <Col sm={12} md={6} lg={4}>
+            <div class="card">
+              <img
+                src="https://media-cdn.laodong.vn/Storage/NewsPortal/2022/2/8/1002117/Ca-Si-Justin-Bieber.jpg"
+                class="card-img-top"
+                alt="..."
+              />
+
+              <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </p>
+                <a href="#" class="btn btn-primary">
+                  Go somewhere
+                </a>
+              </div>
+            </div>
+          </Col>
+          <Col sm={12} md={6} lg={4}>
+            <div class="card">
+              <img
+                src="https://media-cdn.laodong.vn/Storage/NewsPortal/2022/2/8/1002117/Ca-Si-Justin-Bieber.jpg"
+                class="card-img-top"
+                alt="..."
+              />
+
+              <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </p>
+                <a href="#" class="btn btn-primary">
+                  Go somewhere
+                </a>
+              </div>
+            </div>
+          </Col>
+          <Col sm={12} md={6} lg={4}>
+            <div class="card">
+              <img
+                src="https://media-cdn.laodong.vn/Storage/NewsPortal/2022/2/8/1002117/Ca-Si-Justin-Bieber.jpg"
+                class="card-img-top"
+                alt="..."
+              />
+
+              <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </p>
+                <a href="#" class="btn btn-primary">
+                  Go somewhere
+                </a>
+              </div>
+            </div>
+          </Col>
+        </Row>
+        {/* <Row>
+          <Col sm={2} md={4} className="border">
+            sm=true
+          </Col>
+          <Col sm={2} md={4} className="border">
+            sm=true
+          </Col>
+          <Col sm={6} md={4} className="border">
+            sm=true
+          </Col>
+        </Row> */}
+      </Container>
+    </div>
+  );
+};
+
+export default Boostrap;
